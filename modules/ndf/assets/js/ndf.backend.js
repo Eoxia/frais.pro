ee02f85a8d0bded51ce5898f71ae9aa10565624c
Gaël GAUDY <gael.gaudy@eoxia.com>
2017-07-03 10:56:27 +0200

    Add row commit 2

--- modules/ndf/assets/js/ndf.backend.js
+++ modules/ndf/assets/js/ndf.backend.js
@@ -17,7 +17,7 @@ window.eoxiaJS.noteDeFrais.NDF = {};
  */
 window.eoxiaJS.noteDeFrais.NDF.init = function() {
 	jQuery( document ).on( 'click', '.note .close', window.eoxiaJS.noteDeFrais.NDF.closeNDF );
-	jQuery( document ).on( 'click', '.row .add', window.eoxiaJS.noteDeFrais.NDF.addRowNDF );
+	jQuery( document ).on( 'click', '.row.add .action .ion-ios-plus', window.eoxiaJS.noteDeFrais.NDF.addRowNDF );
 };
 
 window.eoxiaJS.noteDeFrais.NDF.openNdf = function( triggeredElement, response ) {
@@ -26,7 +26,6 @@ window.eoxiaJS.noteDeFrais.NDF.openNdf = function( triggeredElement, response ) 
 };
 
 window.eoxiaJS.noteDeFrais.NDF.closeNDF = function( event ) {
-	event.preventDefault();
 	jQuery( '.eox-note-frais' ).removeClass( 'active-single' );
 };
 
@@ -34,6 +33,19 @@ window.eoxiaJS.noteDeFrais.NDF.addNDF = function( triggeredElement, response ) {
 	jQuery( '.single-note' ).html( response.data.view );
 };
 
-window.eoxiaJS.noteDeFrais.NDF.addRowNDF = function() {
+window.eoxiaJS.noteDeFrais.NDF.addRowNDF = function( event ) {
+	var addForm = jQuery( this ).closest( '.row' );
+	var rowClone = addForm.clone();
+	rowClone.removeClass( 'add' );
+	addForm.before( rowClone );
+	addForm.find( 'span[contenteditable="true"]' ).each( function( index ) {
+		jQuery( this ).text( '' );
+	} );
+};
 
+window.eoxiaJS.noteDeFrais.NDF.isModified = function() {
+	jQuery( window ).on( 'beforeunload.edit-post', function() {
+		return true;
+	} );
+	jQuery( window ).trigger( 'beforeunload' );
 };
