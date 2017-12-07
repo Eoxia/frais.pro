06bac70c1dac0ab8a68b751fd018bf862da0e474
Alexandre Techer - Eoxia <alexandre.techer@eoxia.com>
2017-12-07 13:02:44 +0100

    1.4.0: Fix caps check for details route

--- modules/ndf/action/ndf.action.php
+++ modules/ndf/action/ndf.action.php
@@ -203,7 +203,7 @@ class NDF_Action {
 					return $full_note;
 				},
 				'permission_callback' => function() {
-					if ( ( ! in_array( $_SERVER['REMOTE_ADDR'], Config_Util::$init['wpeo_model']->allowed_ip_for_unauthentified_access_rest, true ) ) && ! NDF_Class::g()->check_cap( 'get' ) ) {
+					if ( ( ! in_array( $_SERVER['REMOTE_ADDR'], \eoxia\Config_Util::$init['wpeo_model']->allowed_ip_for_unauthentified_access_rest, true ) ) && ! NDF_Class::g()->check_cap( 'get' ) ) {
 						return false;
 					}
 					return true;
