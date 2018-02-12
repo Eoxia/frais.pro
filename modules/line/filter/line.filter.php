9e05e722500f59f1f8fc1f3a1d9535c8935f0317
Alexandre Techer - Eoxia <alexandre.techer@eoxia.com>
2018-02-12 16:33:08 +0100

    1.4.0: Modification de l'affichage des ligne selon l'affichage

--- modules/line/filter/line.filter.php
+++ modules/line/filter/line.filter.php
@@ -28,10 +28,11 @@ class Line_Filter {
 	 */
 	public function __construct() {
 		add_filter( 'fp_filter_note_line_start', array( $this, 'callback_fp_filter_note_line_start' ), 10, 2 );
+		add_filter( 'fp_filter_note_action_start', array( $this, 'callback_fp_filter_note_action_start' ), 10, 2 );
 	}
 
 	/**
-	 * Filter callback allowing to display some content at the
+	 * Filter callback allowing to display some content at the start of a line in a note.
 	 *
 	 * @param string     $content Current content before calling this filter.
 	 * @param Line_Model $line    Current line definition.
@@ -39,8 +40,27 @@ class Line_Filter {
 	 * @return void
 	 */
 	public function callback_fp_filter_note_line_start( $content, $line ) {
-		if ( 0 <= $line->parent_id ) {
+		if ( 0 >= $line->parent_id ) {
 			\eoxia\View_Util::exec( 'frais-pro', 'line', 'orphelan/checkbox', array(
+				'line' => $line,
+			) );
+			$content = ob_get_clean();
+		}
+
+		echo $content; // WPCS: XSS ok.
+	}
+
+	/**
+	 * Filter callback allowing to display some content at the top of actions on a line
+	 *
+	 * @param string     $content Current content before calling this filter.
+	 * @param Line_Model $line    Current line definition.
+	 *
+	 * @return void
+	 */
+	public function callback_fp_filter_note_action_start( $content, $line ) {
+		if ( 0 < $line->parent_id ) {
+			\eoxia\View_Util::exec( 'frais-pro', 'line', 'dissociation', array(
 				'line' => $line,
 			) );
 			$content = ob_get_clean();
