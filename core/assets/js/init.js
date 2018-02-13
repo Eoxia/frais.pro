d265960e73f6e6e8ac161c3b277b6eb08c1e69b3
Alexandre Techer - Eoxia <alexandre.techer@eoxia.com>
2018-02-13 17:46:44 +0100

    1.4.0: GLobalisation JS dropdown

--- core/assets/js/init.js
+++ core/assets/js/init.js
@@ -1,1 +1,42 @@
 window.eoxiaJS.fraisPro = {};
+
+/**
+ * La méthode appelée automatiquement par la bibliothèque EoxiaJS.
+ *
+ * @return {void}
+ *
+ * @since 1.4.0
+ * @version 1.4.0
+ */
+window.eoxiaJS.fraisPro.init = function() {
+	window.eoxiaJS.fraisPro.event();
+};
+
+/**
+ * Les évènements de la recherche.
+ *
+ * @since 1.4.0
+ * @version 1.4.0
+ *
+ * @return {void}
+ */
+window.eoxiaJS.fraisPro.event = function() {
+	jQuery( document ).on( 'click', '.dropdown-content li', window.eoxiaJS.fraisPro.selectStatus );
+};
+
+/**
+ * Met l'ID du status dans un input caché.
+ *
+ * @since 1.4.0
+ * @version 1.4.0
+ *
+ * @param MouseEvent L'état de la souris au moment du clic
+ *
+ * @return {void}
+ */
+window.eoxiaJS.fraisPro.selectStatus = function( event ) {
+	var parent = jQuery( this ).closest( '.wpeo-dropdown' );
+
+	parent.find( 'input' ).val( jQuery( this ).data( 'id' ) );
+	parent.find( 'button > span' ).html( jQuery( this ).html() );
+};
--- modules/note/assets/js/search.backend.js
+++ modules/note/assets/js/search.backend.js
@@ -27,26 +27,7 @@ window.eoxiaJS.fraisPro.search.init = function() {
  * @return {void}
  */
 window.eoxiaJS.fraisPro.search.event = function() {
-	jQuery( '.bloc-search' ).on( 'click', '.dropdown-content li', window.eoxiaJS.fraisPro.search.selectStatus );
 	jQuery( '.bloc-search' ).on( 'click', '.autocomplete-search-list li', window.eoxiaJS.fraisPro.search.selectUser );
-};
-
-/**
- * Met l'ID du status dans un input caché.
- *
- * @since 1.4.0
- * @version 1.4.0
- *
- * @param MouseEvent L'état de la souri au moment du clic
- *
- * @return {void}
- */
-window.eoxiaJS.fraisPro.search.selectStatus = function( event ) {
-	var parent = jQuery( this ).closest( '.wpeo-dropdown' );
-
-	parent.find( 'input' ).val( jQuery( this ).data( 'id' ) );
-	parent.find( 'span' ).text( jQuery( this ).text() );
-
 };
 
 /**
