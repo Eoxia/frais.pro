c8e72244b2666e5d5c6716cbbf3b7546b29ffd82
eoxia-jimmy <jimmy.eoxia@gmail.com>
2018-02-15 11:16:09 +0100

    1.4.0: Ajout de filtre dans la vue

--- modules/note/view/item.view.php
+++ modules/note/view/item.view.php
@@ -4,7 +4,7 @@
  *
  * @author Eoxia <dev@eoxia.com>
  * @since 1.0.0
- * @version 1.3.0
+ * @version 1.4.0
  * @copyright 2017 Eoxia
  * @package Eoxia/NodeDeFrais
  */
@@ -19,24 +19,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<span class="status-label"><?php echo esc_html( $note->fp_note_status->name ); ?></span>
 	</td>
 	<td class="note-title">
-		<span><?php echo esc_html( $note->title ); ?></span>
+		<span><?php echo esc_html( apply_filters( 'fp_filter_note_item_title', $note->title, $note ) ); ?></span>
 		<div class="note-last-update" ><?php esc_html_e( 'Last update', 'frais-pro' ); ?> : <?php echo esc_html( $note->date_modified['rendered']['date_human_readable'] ); ?></div>
 	</td>
-	<td class="note-ttc">
-		<span class="value"><?php echo esc_html( $note->tax_inclusive_amount ); ?></span>
-		<span class="currency"><?php esc_html_e( '€', 'frais-pro' ); ?></span>
-		<span class="taxe"><?php esc_html_e( 'ATI', 'frais-pro' ); ?></span>
-	</td>
-	<td class="note-tva">
-		<span class="value"><?php echo esc_html( $note->tax_amount ); ?></span>
-		<span class="currency"><?php esc_html_e( '€', 'frais-pro' ); ?></span>
-		<span class="taxe"><?php esc_html_e( 'VAT', 'frais-pro' ); ?></span>
-	</td>
+
+	<?php echo apply_filters( 'fp_filter_note_item_informations', $note, 'table' ); ?>
+
 	<td class="note-action">
-		<span class="export toggle list" data-parent="toggle" data-target="content">
-			<?php \eoxia\View_Util::exec( 'frais-pro', 'note', 'actions', array(
-				'note' => $note,
-			) ); ?>
-		</span>
+		<?php echo apply_filters( 'fp_filter_note_item_actions', $note ); ?>
 	</td>
 </tr>
