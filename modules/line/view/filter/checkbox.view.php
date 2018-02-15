09e4595d13456262bb1a3e54545f4f296a0a719a
eoxia-jimmy <jimmy.eoxia@gmail.com>
2018-02-15 16:13:48 +0100

    1.4.0: Initialises les fichiers: checkbox, note-unaffected et results

---
+++ modules/line/view/filter/checkbox.view.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Affichage de la checkbox.
+ *
+ * @author Eoxia <dev@eoxia.com>
+ * @since 1.4.0
+ * @version 1.4.0
+ * @copyright 2017 Eoxia
+ * @package Frais.pro
+ */
+
+namespace frais_pro;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?><div class="table-cell line-association-selection">
+	<input type="checkbox" value="<?php echo esc_attr( $line->id ); ?>" name="line_to_affect[]" />
+</div>
