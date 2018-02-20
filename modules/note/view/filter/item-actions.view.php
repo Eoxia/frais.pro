cfb5b299f8f7e2c14e1c955351a5e35ef8d82690
eoxia-jimmy <jimmy.eoxia@gmail.com>
2018-02-20 15:56:42 +0100

    1.4.0: Ajout de last document

--- modules/note/view/filter/item-actions.view.php
+++ modules/note/view/filter/item-actions.view.php
@@ -39,23 +39,28 @@ if ( ! defined( 'ABSPATH' ) ) {
 </div>
 <div class="wpeo-dropdown dropdown-right" >
 	<button class="dropdown-toggle wpeo-button button-main button-square-40 button-rounded wpeo-tooltip-event" aria-label="<?php esc_html_e( 'Download file', 'frais-pro' ); ?>" ><i class="button-icon far fa-arrow-to-bottom" ></i></button>
-	<ul class="dropdown-content" >
-		<li class="dropdown-item action-attribute wpeo-tooltip-event"
-			data-id="<?php echo esc_attr( $note->id ); ?>"
-			data-action="download_export"
-			aria-label="Fichier non généré"
-			data-direction="left"
-			data-nonce="<?php echo esc_attr( wp_create_nonce( 'download_export' ) ); ?>"
-			data-picture="1"><i class="icon fa-fw fas fa-file-image"></i>&nbsp;<?php esc_html_e( 'With picture', 'frais-pro' ); ?></li>
-		<li class="dropdown-item action-attribute"
-			data-id="<?php echo esc_attr( $note->id ); ?>"
-			data-action="download_export"
-			data-nonce="<?php echo esc_attr( wp_create_nonce( 'download_export' ) ); ?>"
-			data-picture="0"><i class="icon fa-fw fas fa-file"></i>&nbsp;<?php esc_html_e( 'Without picture', 'frais-pro' ); ?></li>
-		<li class="dropdown-item action-attribute"
-			data-id="<?php echo esc_attr( $note->id ); ?>"
-			data-action="download_export"
-			data-nonce="<?php echo esc_attr( wp_create_nonce( 'download_export' ) ); ?>"><i class="icon fa-fw fas fa-file-excel"></i>&nbsp;<?php esc_html_e( 'CSV export', 'frais-pro' ); ?></li>
+	<ul class="dropdown-content">
+		<li class="dropdown-item wpeo-tooltip-event"
+			aria-label="<?php echo esc_attr( $note['last_document']['note-photo']['tooltip'] ); ?>"
+			data-direction="left">
+			<a href="<?php echo esc_attr( $note['last_document']['note-photo']['file_informations']['link'] ); ?>">
+				<i class="icon fa-fw fas fa-file-image"></i>&nbsp;<?php esc_html_e( 'With picture', 'frais-pro' ); ?>
+			</a>
+		</li>
+		<li class="dropdown-item wpeo-tooltip-event"
+			aria-label="<?php echo esc_attr( $note['last_document']['note']['tooltip'] ); ?>"
+			data-direction="left">
+			<a href="<?php echo esc_attr( $note['last_document']['note']['file_informations']['link'] ); ?>">
+				<i class="icon fa-fw fas fa-file"></i>&nbsp;<?php esc_html_e( 'Without picture', 'frais-pro' ); ?>
+			</a>
+		</li>
+		<li class="dropdown-item wpeo-tooltip-event"
+			aria-label="<?php echo esc_attr( $note['last_document']['note-csv']['tooltip'] ); ?>"
+			data-direction="left">
+			<a href="<?php echo esc_attr( $note['last_document']['note-csv']['file_informations']['link'] ); ?>">
+				<i class="icon fa-fw fas fa-file-excel"></i>&nbsp;<?php esc_html_e( 'CSV export', 'frais-pro' ); ?>
+			</a>
+		</li>
 	</ul>
 </div>
 <div class="wpeo-dropdown dropdown-right" >
