75fdbc9a6e0eaa5b08927851777ab5c2f355c943
Jimmy <jimmy.eoxia@gmail.com>
2019-11-21 11:30:27 +0100

    Ajout de la clé mode dev

--- frais-pro.php
+++ frais-pro.php
@@ -19,7 +19,10 @@ namespace frais_pro;
 DEFINE( 'PLUGIN_NOTE_DE_FRAIS_PATH', realpath( plugin_dir_path( __FILE__ ) ) . '/' );
 DEFINE( 'PLUGIN_NOTE_DE_FRAIS_URL', plugins_url( basename( __DIR__ ) ) . '/' );
 DEFINE( 'PLUGIN_NOTE_DE_FRAIS_DIR', basename( __DIR__ ) );
+DEFINE( 'PLUGIN_NOTE_DE_FRAIS_DEV_MODE', true );
 
-require_once 'core/external/eo-framework/eo-framework.php';
+if ( ! PLUGIN_NOTE_DE_FRAIS_DEV_MODE ) {
+	require_once 'core/external/eo-framework/eo-framework.php';
+}
 
 \eoxia\Init_Util::g()->exec( PLUGIN_NOTE_DE_FRAIS_PATH, basename( __FILE__, '.php' ) );
