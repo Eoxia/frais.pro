362fb829bd09bbd7f9246644ce97c34721edde99
Jimmy <jimmy.eoxia@gmail.com>
2019-11-21 11:34:07 +0100

    Gestion du header différent

--- core/view/main-single.view.php
+++ core/view/main-single.view.php
@@ -16,19 +16,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 } ?>
 
 <div class="wrap-frais-pro">
-	<div class="content-wrap single-note">
-
-		<?php \eoxia\View_Util::exec( 'frais-pro', 'core', 'main-header-single' ); ?>
-
-		<div class="wrap wpeo-wrap" >
-			<?php if ( ! empty( $waiting_updates ) && strpos( $_SERVER['REQUEST_URI'], 'admin.php' ) && ! strpos( $_SERVER['REQUEST_URI'], 'admin.php?page=' . \eoxia\Config_Util::$init['frais-pro']->update_page_url ) ) : ?>
-				<?php \eoxia\View_Util::exec( 'frais-pro', 'update_manager', 'say-to-update' ); ?>
-			<?php else : ?>
-				<?php Note_Class::g()->display_single(); ?>
-				<?php if ( ! User_Class::g()->check_required_fields( $user ) ) : ?>
-					<?php User_Class::g()->display_update_modal_message(); ?>
-				<?php endif; ?>
-			<?php endif; ?>
-		</div>
-	</div>
+	<?php if ( ! empty( $waiting_updates ) && strpos( $_SERVER['REQUEST_URI'], 'admin.php' ) && ! strpos( $_SERVER['REQUEST_URI'], 'admin.php?page=' . \eoxia\Config_Util::$init['frais-pro']->update_page_url ) ) : ?>
+		<?php \eoxia\View_Util::exec( 'frais-pro', 'update_manager', 'say-to-update' ); ?>
+	<?php else : ?>
+		<?php Note_Class::g()->display_single( $note->data['id'] ); ?>
+		<?php if ( ! User_Class::g()->check_required_fields( $user ) ) : ?>
+			<?php User_Class::g()->display_update_modal_message(); ?>
+		<?php endif; ?>
+	<?php endif; ?>
 </div>
