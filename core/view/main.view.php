70e261c99ff2d75e691552bcca6e668120674aa1
Jimmy <jimmy.eoxia@gmail.com>
2019-11-21 11:32:20 +0100

    Passages en LF

--- core/view/main.view.php
+++ core/view/main.view.php
@@ -15,19 +15,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 } ?>
 
-<div class="content-wrap">
-	
-	<?php \eoxia\View_Util::exec( 'frais-pro', 'core', 'main-header' ); ?>
-
-	<div class="wrap wpeo-wrap wrap-frais-pro">
-		<?php if ( ! empty( $waiting_updates ) && strpos( $_SERVER['REQUEST_URI'], 'admin.php' ) && ! strpos( $_SERVER['REQUEST_URI'], 'admin.php?page=' . \eoxia\Config_Util::$init['frais-pro']->update_page_url ) ) : ?>
-			<?php \eoxia\Update_Manager_Class::g()->display_say_to_update( 'frais-pro', __( 'Need to update Frais.pro data', 'frais-pro' ) ); ?>
-		<?php else : ?>
-			<?php Note_Class::g()->display(); ?>
-			<?php if ( ! User_Class::g()->check_required_fields( $user ) ) : ?>
-				<?php User_Class::g()->display_update_modal_message(); ?>
-			<?php endif; ?>
+<div class="wrap wpeo-wrap wrap-frais-pro">
+	<?php if ( ! empty( $waiting_updates ) && strpos( $_SERVER['REQUEST_URI'], 'admin.php' ) && ! strpos( $_SERVER['REQUEST_URI'], 'admin.php?page=' . \eoxia\Config_Util::$init['frais-pro']->update_page_url ) ) : ?>
+		<?php \eoxia\Update_Manager_Class::g()->display_say_to_update( 'frais-pro', __( 'Need to update Frais.pro data', 'frais-pro' ) ); ?>
+	<?php else : ?>
+		<?php Note_Class::g()->display(); ?>
+		<?php if ( ! User_Class::g()->check_required_fields( $user ) ) : ?>
+			<?php User_Class::g()->display_update_modal_message(); ?>
 		<?php endif; ?>
-	</div>
-
+	<?php endif; ?>
 </div>
